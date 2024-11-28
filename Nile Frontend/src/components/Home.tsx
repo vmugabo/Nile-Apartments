3b32035c18f6a782b9f136446e654ea935c8fc22
michaelmusembi <m.musembi@alustudent.com>
2024-11-29 00:33:06 +0300

    An update of the main

--- Nile Frontend/src/components/Home.tsx
+++ Nile Frontend/src/components/Home.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Home: React.FC = () => (
   <div className="container mt-4">
     <h2>Welcome to Nile Apartments</h2>
+    <p>We ofer luxury serviced apartments near Kigali International Airport, offering premium comfort for business travelers and tourists alike. </p>
     <p>Navigate to the dashboard to see your reservations.</p>
   </div>
 );
